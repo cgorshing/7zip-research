c912a3754cadc14cc69491bf034fbe009de06d22
Chad Gorshing <gorshing@gmail.com>
2012-01-05 11:41:59 -0600

    Not quite to finding the filename, but packed and unpacked size at least.

--- src/research/SevenZipDecoder.java
+++ src/research/SevenZipDecoder.java
@@ -6,13 +6,16 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
 public class SevenZipDecoder {
-  public static final int kEnd = 0x00;
-  public static final int kHeader = 0x01;
-  public static final int kAdditionalStreamsInfo = 0x03;
-  public static final int kMainStreamsInfo = 0x04;
-  public static final int kPackInfo = 0x06;
-  public static final int kSize = 0x09;
-  public static final int kCrc = 0x0A;
+  public static final int kEnd0x00 = 0x00;
+  public static final int kHeader0x01 = 0x01;
+  public static final int kAdditionalStreamsInfo0x03 = 0x03;
+  public static final int kMainStreamsInfo0x04 = 0x04;
+  public static final int kPackInfo0x06 = 0x06;
+  public static final int kUnPackINfo0x07 = 0x07;
+  public static final int kSubStreamsInfo0x08 = 0x08;
+  public static final int kSize0x09 = 0x09;
+  public static final int kCrc0x0A = 0x0A;
+  public static final int kFolder0x0B = 0x0B;
 
   public static void main(String[] args) throws IOException {
     FileInputStream inputStream = new FileInputStream("C:\\tmp\\chad.7z");
@@ -59,37 +62,96 @@ public class SevenZipDecoder {
 
     long skip = inputStream.skip(nextHeaderOffset);
 
-    int possibleHeader = inputStream.read();
+    assert kHeader0x01 == inputStream.read();
+    assert kMainStreamsInfo0x04 == inputStream.read();
+    assert kPackInfo0x06 == inputStream.read();
 
-    assert possibleHeader == kHeader;
+    long packOffset = read7ZipUInt64(inputStream);
+    long numberOfStreams = read7ZipUInt64(inputStream);
 
-    int next = inputStream.read();
-    assert next == kMainStreamsInfo;
+    assert numberOfStreams == 1;
 
-    //byte [] mainStreamsInfo = new byte[]
-    next = inputStream.read();
-    assert next == kPackInfo;
+    assert kSize0x09 == inputStream.read();
 
-    long foo = read7ZipUInt64(inputStream);
+    for (int i = 0; i < numberOfStreams; ++i) {
+      long packsize = read7ZipUInt64(inputStream);
+
+      assert packsize == 3307 : packsize;
+    }
+
+    assert kEnd0x00 == inputStream.read(); // This ends the PackInfo section
+
+    //figure out what optional section is next
+    assert kUnPackINfo0x07 == inputStream.read();
+    assert kFolder0x0B == inputStream.read();
+    long numOfFolders = read7ZipUInt64(inputStream);
+    assert 1 == numOfFolders;
+    int external = inputStream.read();
+    if (external == 0) {
+      //Folders[NumFolders]
+      assert 1 == inputStream.read(); // Num of Coders;
+      int val = inputStream.read();
+      int codecIdSize = 0x0F & val;
+      boolean isComplexCoder = (0x10 & val) != 0;
+      boolean areAttributes = (0x20 & val) != 0;
+
+      byte [] codecId = new byte[codecIdSize];
+      assert codecIdSize == inputStream.read(codecId);
+
+      if (isComplexCoder) {
+        throw new RuntimeException("Not sure what to do with this.");
+      }
+
+      if (areAttributes) {
+        int propertiesSize = (int)read7ZipUInt64(inputStream);
+        byte [] properties = new byte[propertiesSize];
+        assert propertiesSize == inputStream.read(properties);
+      }
+    }
+    else if (external == 1) {
+      //UINT64 DataStreamIndex
+    }
+
+    assert 0x0C == inputStream.read();
+
+    for (int i = 0; i < numOfFolders; ++i) {
+      for (int j = 0; j < numberOfStreams; ++j) {
+        long l = read7ZipUInt64(inputStream);
+        assert 4242 == l : l;
+      }
+    }
+
+    assert kEnd0x00 == inputStream.read();
+    assert kSubStreamsInfo0x08 == inputStream.read();
+
+    int nextSection = inputStream.read();
+    if (nextSection == kCrc0x0A) {
+
+    }
+    else {
+      throw new RuntimeException("Unimplemented");
+    }
 
     inputStream.close();
   }
 
   private static long read7ZipUInt64(FileInputStream inputStream) throws IOException {
-    byte firstByte = (byte)inputStream.read();
-    byte mask = (byte)0x80;
+    //ThrowEndOfData();
+    int firstByte = inputStream.read();
+    int mask = 0x80;
     long value = 0;
     for (int i = 0; i < 8; ++i) {
       if ((firstByte & mask) == 0) {
-        long highPart = firstByte & (mask - 1);
+        int highPart = ((firstByte & 0xFF) & (mask - 1));
         value += (highPart << (i * 8));
         return value;
       }
 
       //ThrowEndOfData();
-      long nextByte = (long)inputStream.read();
-      value |= (nextByte << (8 * i));
-      mask >>= 1;
+      int nextByte = inputStream.read();
+      value |= (nextByte & 0xFF) << (8 * i);
+
+      mask >>>= 1;
     }
     return value;
   }
