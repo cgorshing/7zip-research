ec5d5a42927afdf8ccde62a28cb2ff713e091a51
Chad Gorshing <gorshing@gmail.com>
2013-02-22 09:42:11 -0600

    Added Crc.java

---
+++ src/main/java/research/Crc.java
@@ -0,0 +1,40 @@
+package research;
+
+/**
+ * From http://www.java2s.com/Code/Java/Security/CRCfrom7zip.htm
+ */
+public class Crc {
+    static public int[] Table = new int[256];
+    int _value = -1;
+
+    static {
+        for (int i = 0; i < 256; ++i) {
+            int r = i;
+            for (int j = 0; j < 8; ++j)
+                if ((r & 1) != 0)
+                    r = (r >>> 1) ^ 0xEDB88320;
+                else
+                    r >>>= 1;
+            Table[i] = r;
+        }
+    }
+
+    public void Update(byte[] data, int offset, int size) {
+        for (int i = 0; i < size; ++i)
+            _value = Table[(_value ^ data[offset + i]) & 0xFF] ^ (_value >>> 8);
+    }
+
+    public void Update(byte[] data) {
+        int size = data.length;
+        for (int i = 0; i < size; ++i)
+            _value = Table[(_value ^ data[i]) & 0xFF] ^ (_value >>> 8);
+    }
+
+    public void UpdateByte(int b) {
+        _value = Table[(_value ^ b) & 0xFF] ^ (_value >>> 8);
+    }
+
+    public int GetDigest() {
+        return _value ^ -1;
+    }
+}
